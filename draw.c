796b921f573ab52edc2cd8d68e6ff6aa17f0c2ff
jonathanbuchanan <jonathan.russ.buchanan@gmail.com>
2017-08-31 15:51:04 -0400

    The command window is drawn

--- draw.c
+++ draw.c
@@ -5,16 +5,22 @@
 struct Window {
     WINDOW *term;
     WINDOW *tab;
+    WINDOW *cmd;
 };
 
 // Creates the window for the tab editor
 void init_tab_window(struct Window *window);
 void draw_tab_window(struct Window *window);
 
+// Creates the window for the command line
+void init_cmd_window(struct Window *window);
+void draw_cmd_window(struct Window *window);
+
 struct Window * init_window() {
     struct Window *w = malloc(sizeof(struct Window));
     w->term = initscr();
     init_tab_window(w);
+    init_cmd_window(w);
     draw_tab_window(w);
     return w;
 }
@@ -31,6 +37,18 @@ void init_tab_window(struct Window *window) {
     wborder(tab, ' ', ' ', ' ', '_', ' ', ' ', '_', '_');
     wrefresh(tab);
     window->tab = tab;
+}
+
+#define CMD_WINDOW_HEIGHT 2
+void init_cmd_window(struct Window *window) {
+    int y, x;
+    getmaxyx(window->term, y, x);
+    WINDOW *cmd = newwin(CMD_WINDOW_HEIGHT, x, y - CMD_WINDOW_HEIGHT, 0);
+    start_color();
+    init_pair(1, COLOR_BLACK, COLOR_WHITE);
+    wbkgd(cmd, COLOR_PAIR(1));
+    wrefresh(cmd);
+    window->cmd = cmd;
 }
 
 
